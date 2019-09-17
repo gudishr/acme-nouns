65e04bfd8a1afe2333df85d4617a7c2496d114e5
shruti gudi <shrutigudi@gmail.com>
2019-09-16 20:02:32 -0700

    added table places things

--- db.js
+++ db.js
@@ -7,6 +7,35 @@ const People = conn.define('people', {
   name: {
     type: STRING,
     allowNull : false,
+    notEmpty : true,
+    unique: true
+  },
+  id : {
+    type : UUID,
+    primaryKey : true,
+    defaultValue : UUIDV4
+  }
+})
+
+const Places = conn.define('places', {
+  name: {
+    type: STRING,
+    allowNull : false,
+    notEmpty : true,
+    unique: true
+  },
+  id : {
+    type : UUID,
+    primaryKey : true,
+    defaultValue : UUIDV4
+  }
+})
+
+const Things = conn.define('things', {
+  name: {
+    type: STRING,
+    allowNull : false,
+    notEmpty : true,
     unique: true
   },
   id : {
