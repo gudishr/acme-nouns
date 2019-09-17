e173bf52a7a900172c25f81a9f1db0d2b07fab27
shruti gudi <shrutigudi@gmail.com>
2019-09-16 21:06:57 -0700

    set up /api/people

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const app = express()
 const db = require('./db')
+const { People, Places, Things } = db.models
 
 const port = process.env.port || 3000
+
+app.get('/api/people', (req, res, next) => {
+  People.findAll()
+    .then( people => res.send(people))
+    .catch(next)
+})
+
 
 db.syncAndSeed()
   .then(() => {
