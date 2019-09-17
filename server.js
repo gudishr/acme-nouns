9ba3fae6ec4b235274ba442ca75274cc18c42572
shruti gudi <shrutigudi@gmail.com>
2019-09-16 21:12:11 -0700

    added / for index.html

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ const express = require('express')
 const app = express()
 const db = require('./db')
 const { People, Places, Things } = db.models
+const path = require('path')
 
 const port = process.env.port || 3000
+
+app.get('/', (req, res, next) =>{
+  res.sendFile(path.join(__dirname, 'index.html'))
+})
 
 app.get('/api/people', (req, res, next) => {
   People.findAll()
